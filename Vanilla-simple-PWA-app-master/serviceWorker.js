991f3d7b1b91f4e62d084f39b27c84da40b557b6
kariman omar <liv8557@gmail.com>
2023-09-19 14:30:20 +0200

    PWA-assignmen

---
+++ Vanilla-simple-PWA-app-master/serviceWorker.js
@@ -0,0 +1,42 @@
+self.addEventListener("install",(event)=>{
+    console.log("installed")
+    event.waitUntil(
+        caches.open("simpleApp-1").then(cache=>{
+            return cache.addAll(["index.html","styles/index.css","js/main.js","other.html","styles/other.css","js/other.js"])
+        }).catch((err)=>{console.log(err)})
+    )
+})
+
+self.addEventListener("activate",()=>{
+    console.log("activate")
+})
+
+self.addEventListener("fetch",(event)=>{
+    console.log(event.request.url);
+    event.respondWith(
+        caches.match(event.request).then((file)=>{
+                        if(file){
+                            console.log("Found cache",event.request.url);
+                            return file
+                        }
+                        console.log("not Found cache :",event.request.url);
+                       return fetch(event.request.url)
+            
+                    }).catch((err)=>{console.log(err)})
+    )
+})
+// self.addEventListener("fetch",(event)=>{
+//     console.log("Network request :",event.request.url);
+//     event.respondWith(
+//         caches.match(event.request).then((file)=>{
+//             if(file){
+//                 console.log("Found in cache",event.request.url);
+//                 return file
+//             }
+//             console.log("Network request :",event.request.url);
+//            return fetch(event.request.url)
+
+//         }).catch(err=>console.log(err))
+//     )
+
+// })
